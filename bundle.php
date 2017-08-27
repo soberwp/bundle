394dd231683d67c7ac75c3fca54c49ca6d2759ef
Darren Jacoby <darren@jacoby.co.za>
2017-08-27 12:28:19 +0200

    Composer package

--- bundle.php
+++ bundle.php
@@ -1,28 +1,8 @@
 <?php
-/*
-Plugin Name:        Bundle
-Plugin URI:         http://github.com/soberwp/bundle
-Description:        WordPress plugin to enable plugin activation using a JSON, YAML or PHP file
-Version:            1.0.1
-Author:             Sober
-Author URI:         http://github.com/soberwp/
-License:            MIT License
-License URI:        http://opensource.org/licenses/MIT
-GitHub Plugin URI:  soberwp/bundle
-GitHub Branch:      master
-*/
+
 namespace Sober\Bundle;
 
 use Sober\Bundle\Loader;
-
-/**
- * Plugin
- */
-if (!defined('ABSPATH')) {
-    die;
-};
-
-require(file_exists($composer = __DIR__ . '/vendor/autoload.php') ? $composer : __DIR__ . '/dist/autoload.php');
 
 /**
  * Hook
